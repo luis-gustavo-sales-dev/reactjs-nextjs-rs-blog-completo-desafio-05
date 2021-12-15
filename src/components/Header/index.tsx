52d5bb9490493bf3b7f78be92ccf2cc1ad7baae1
Luis Gustavo <luisgssf@gmail.com>
2021-12-15 00:01:26 -0300

    Componente Header criado e inserido

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,12 @@
-export default function Header() {
+import styles from './styles.module.scss';
+
+export default function Header(): JSX.Element {
   // TODO
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="logo" />
+      </div>
+    </header>
+  );
 }
