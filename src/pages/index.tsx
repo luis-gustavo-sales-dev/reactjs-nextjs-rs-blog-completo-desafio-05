cc2e6312a32395bd87e30ff50033779297015d66
Luis Gustavo <luisgssf@gmail.com>
2021-12-15 01:27:26 -0300

    Posts are retrieved from Prismic backend.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable prettier/prettier */
 import { GetStaticProps } from 'next';
-
+import { RichText } from 'prismic-dom';
+import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../services/prismic';
-
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
@@ -24,13 +27,49 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
-  return <h1>Home</h1>;
+export default function Home({ postsPagination }: HomeProps) {
+  return (
+    postsPagination.results.map ( post => {
+      return <li>{post.data.title}</li>
+    })
+  );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query([
+    Prismic.predicates.at('document.type', 'posts')
+  ], {
+    fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+    pageSize: 10,
+  });
 
-//   // TODO
-// };
+  // console.log(JSON.stringify(postsResponse.results, null, 4))
+  const posts = postsResponse.results.map( (post: Post) => {
+    return  {
+      slug: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author
+      }
+    }
+  })
+
+  // console.log(posts)
+
+  const postsPagination: PostPagination = {
+    next_page: postsResponse.next_page,
+    results: [...posts]
+  }
+
+  // console.log(postsPagination)
+
+  return {
+    props: {
+      postsPagination,
+    },
+  };
+  // TODO
+};
